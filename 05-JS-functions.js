8e35cb79136276d2a50287a4b6d39dbd52552dcd
Mostafa El-Marzouki <mostafam.elmarzouki@gmail.com>
2017-10-08 13:47:16 +0200

    js functions added

--- 05-JS-functions.js
+++ 05-JS-functions.js
@@ -3,3 +3,21 @@
     JavaScript Functions 
     =====================
 */
+function rectanglePerimeter(Height, Width) {
+    return Height * Width;
+}
+
+var x = rectanglePerimeter(4, 3);
+console.log("rectanglePerimeter = " + x);
+
+// The () Operator Invokes the Function
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+
+// Functions Used as Variable Values
+var x = toCelsius(77);
+var text = "The temperature is " + x + " Celsius";
+console.log(text);
+var text = "The temperature is " + toCelsius(77) + " Celsius";
+console.log(text);
